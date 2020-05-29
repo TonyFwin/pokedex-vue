bdaa2241a537c3e62e329bf2846f331a41e25864
Tony Nguyen <tonynguyen201@gmail.com>
2020-05-29 09:10:11 -0500

    extract out header into component

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>
+      <router-link to="/">{{ title }}</router-link>
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String
+  },
+  data() {
+    return { show: false };
+  }
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #111;
+  padding: 20px;
+  color: white;
+}
+
+h1 {
+  margin: 0;
+}
+</style>
