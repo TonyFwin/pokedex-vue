6c3c2d4ba733257726b5807d1cb60116b3db9f4e
Tony Nguyen <tonynguyen201@gmail.com>
2020-05-29 09:41:06 -0500

    initial pokemon detail setup

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -1,14 +1,47 @@
 <template>
   <div>
-    <h1>Pokemon Detail</h1>
-    <h2>{{$route.params.id}}</h2>
+    <h1>{{pokemon.name}}</h1>
+    <img class="m-auto" :src="pokemon.sprites.front_default" alt="pokemon.name" />
+    <div>
+      <h2>Abilities</h2>
+      <ul>
+        <li v-for="(ability,i) in pokemon.abilities" :key="i">{{ability.ability.name}}</li>
+      </ul>
+    </div>
+    <div>
+      <h2>Type(s)</h2>
+      <ul>
+        <li v-for="(type, i) in pokemon.types" :key="i">{{type.type.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
 export default {
-  name: "PokemonDetail"
+  name: "PokemonDetail",
+  props: ["pokeindex"],
+  data: function() {
+    return {
+      pokemon: {}
+    };
+  },
+  created: function() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function() {
+      try {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${this.$route.params.id}`
+        );
+        const data = await res.json();
+        this.pokemon = data;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 };
 </script>
 
