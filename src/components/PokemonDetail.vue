4076582641241dcfaa0ee4daa39a5c20f6fa73a2
Tony Nguyen <tonynguyen201@gmail.com>
2020-05-31 22:15:40 -0500

    updated layout skeleton

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -1,46 +1,53 @@
 <template>
-  <div class="mt-3">
-    <h1>{{pokemon.name}}</h1>
-    <div class="flex flex-wrap">
-      <div class="sprites">
+  <div v-if="!loading" class="mt-3">
+    <div class="details-top">
+      <h1>{{pokemon.name}}</h1>
+      <div>
+        <div>
+          <span v-for="(type, i) in pokemon.types" :key="i">{{type.type.name}}</span>
+        </div>
+        <div>#{{$route.params.id}}</div>
+      </div>
+    </div>
+
+    <div class="detail-header flex flex-wrap">
+      <div class="detail-sprites">
         <div class="flex justify-center">
           <img :src="pokemon.sprites.front_default" alt="pokemon.name" />
-          <img class :src="pokemon.sprites.back_default" alt="pokemon.name" />
         </div>
-        <div class="flex justify-center">
-          <img :src="pokemon.sprites.front_shiny" alt />
-          <img :src="pokemon.sprites.back_shiny" alt />
-        </div>
-      </div>
-      <div class="stats">
-        <h2>Stats</h2>
-        <ul class="statList">
-          <li
-            v-for="(stat,i) in pokemon.stats"
-            :key="i"
-          >{{formatStat(stat.stat.name)}}: {{stat.base_stat}}</li>
-        </ul>
       </div>
     </div>
+
     <div class="w-full">
       <h2>Description</h2>
-      <p>{{flavorText}}</p>
+      <p>{{speciesData.flavor_text_entries[1].flavor_text}}</p>
     </div>
-    <div class="flex flex-wrap">
+    <div>
+      <h2>About</h2>
       <div class>
-        <h2>Abilities</h2>
-        <ul>
-          <li v-for="(ability,i) in pokemon.abilities" :key="i">{{formatStat(ability.ability.name)}}</li>
-        </ul>
+        <p>Genus: {{speciesData.genera[2].genus}}</p>
+        <p>Height: {{pokemon.height}} cm</p>
+        <p>Weight: {{pokemon.weight}} kg</p>
+        <p>
+          Abilities:
+          <span
+            v-for="(ability,i) in pokemon.abilities"
+            :key="i"
+          >{{formatStat(ability.ability.name)}}</span>
+        </p>
       </div>
-      <div class>
-        <h2>Type(s)</h2>
-        <ul>
-          <li v-for="(type, i) in pokemon.types" :key="i">{{type.type.name}}</li>
-        </ul>
-      </div>
+    </div>
+    <div class="detail-stats">
+      <h2>Stats</h2>
+      <ul class="statList">
+        <li
+          v-for="(stat,i) in pokemon.stats"
+          :key="i"
+        >{{formatStat(stat.stat.name)}}: {{stat.base_stat}}</li>
+      </ul>
     </div>
   </div>
+  <div v-else>Loading...</div>
 </template>
 
 <script>
@@ -48,13 +55,15 @@ export default {
   name: "PokemonDetail",
   data: function() {
     return {
+      loading: true,
       pokemon: {},
-      flavorText: {}
+      speciesData: {}
     };
   },
   created: function() {
     this.fetchData();
-    this.fetchFlavorText();
+    this.fetchSpeciesData();
+    this.loading = false;
   },
   methods: {
     fetchData: async function() {
@@ -68,13 +77,13 @@ export default {
         console.log(e);
       }
     },
-    fetchFlavorText: async function() {
+    fetchSpeciesData: async function() {
       try {
         const res = await fetch(
           `https://pokeapi.co/api/v2/pokemon-species/${this.$route.params.id}`
         );
         const data = await res.json();
-        this.flavorText = data.flavor_text_entries[1].flavor_text;
+        this.speciesData = data;
       } catch (e) {
         console.log(e);
       }
@@ -91,27 +100,29 @@ h1,
 li {
   text-transform: capitalize;
 }
-.sprites,
-.stats {
+.detail-sprites,
+.detail-stats {
   max-width: 100%;
   flex: 0 0 100%;
 }
 
 @media screen and (min-width: 550px) {
-  ul {
-    grid-template-columns: repeat(3, 1fr);
-  }
 }
 @media screen and (min-width: 800px) {
-  .sprites,
-  .stats {
+  .detail-sprites,
+  .detail-stats {
     max-width: 50%;
     flex: 0 0 50%;
   }
 }
 @media screen and (min-width: 1200px) {
-  ul {
-    grid-template-columns: repeat(6, 1fr);
+  .detail-sprites {
+    max-width: 30%;
+    flex: 0 0 30%;
+  }
+  .detail-stats {
+    max-width: 70%;
+    flex: 0 0 70%;
   }
 }
 </style>
